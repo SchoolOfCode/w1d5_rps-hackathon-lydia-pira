68a907b00d830b851fbb9ca9ea146bbe6f2a3e06
sitaaraa <lnicola@gmail.com>
2022-04-29 10:14:49 +0100

    Task 1 if statements testing

---
+++ main.js
@@ -0,0 +1,33 @@
+let playerMove;
+let computerMove;
+
+
+if (playerMove === "rock") { 
+    if computerMove === "rock" {
+    console.log("draw");
+} else if (computerMove === "paper") {
+    console.log("loss");
+} else if (computerMove === "scissors") {
+    console.log("win");
+}
+};
+
+if (playerMove === "scissors") { 
+    if computerMove === "scissors" {
+    console.log("draw");
+} else if (computerMove === "rock") {
+    console.log("loss");
+} else if (computerMove === "paper") {
+    console.log("win");
+}
+};
+
+if (playerMove === "paper") { 
+    if computerMove === "paper" {
+    console.log("draw");
+} else if (computerMove === "scissors") {
+    console.log("loss");
+} else if (computerMove === "rock") {
+    console.log("win");
+}
+};
